15246d9fbcdcf823d392b20ed32cec7354a0a76e
Murilo Cruz <murilomecr@gmail.com>
2025-08-09 14:20:56 -0300

    v1.2

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
 import lottie from 'lottie-web';
 import iconWhatsapp from './assets/icon-whatsapp.json?url';
 
-// const logoEl = document.querySelector<HTMLElement>('#toolbar--logo')!;
 const heroCtaWpp = document.querySelector<HTMLElement>('#hero--cta--icon')!;
 const footerCtaWpp = document.querySelector<HTMLElement>('#footer--cta--icon')!;
-
-// logoEl.addEventListener('click', (event) => {
-//   event.preventDefault();
-// });
 
 lottie.loadAnimation({
     container: heroCtaWpp,
@@ -24,3 +19,15 @@ lottie.loadAnimation({
     autoplay: true,
     path: `${iconWhatsapp}`
 });
+
+const btnGetInTouch = document.querySelectorAll<HTMLButtonElement>('.btnGetInTouch');
+
+if (btnGetInTouch) {
+    btnGetInTouch?.forEach(btn => btn.addEventListener('click', getInTouch));
+}
+
+function getInTouch() {
+    const whatsappURL = `https://wa.me/${5515988334964}?text=${`Olá, quero agendar um atendimento!`}`;
+
+    window.open(whatsappURL, '_blank', 'noopener,noreferrer');
+}
